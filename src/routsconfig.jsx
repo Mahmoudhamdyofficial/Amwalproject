aaf638de6e2542c0f0582d5ce780b62ac04cdef0
mahmoudhamdy <mahmoud.hamdy9993@gmail.com>
2024-11-28 15:53:44 +0200

    edit

--- src/routsconfig.jsx
+++ src/routsconfig.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Applayout from './components/applayout/applayout';
 import Formone from './components/formone';
 import Form2 from './components/form2';
 import Form3 from './components/form3';
